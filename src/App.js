fa46f98e34138bec2cfe137cf7440f7db69043bb
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-05-05 15:15:54 +0300

    created and style main navvigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World!
-      </div>
-    )
-  }
-}
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MainNav from "./MainNav/MainNav";
+import Home from "./Home/Home";
+import About from "./About/About";
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <MainNav />
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </main>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
