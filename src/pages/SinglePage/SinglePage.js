75fe496706e558a73f9d61eca789eca902e0f6d7
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-05-07 16:52:14 +0300

    created delete function for single experience, which also when click back to experience button also update the landing page

--- src/pages/SinglePage/SinglePage.js
+++ src/pages/SinglePage/SinglePage.js
@@ -1,45 +1,91 @@
-import React from 'react';
-import postsdata from '../../components/postsdata';
-import './SinglePage.css';
+import React, { useState, useEffect } from "react";
+import "./SinglePage.css";
+import axios from "axios";
 
 import { useParams, Link } from "react-router-dom";
 
-
-import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
-import Button from 'react-bootstrap/Button'
-
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 
 const SinglePage = () => {
+  const [loadedExp, setLoadedExp] = useState();
+  let { postId } = useParams();
+  const deleteHandler = (id) => {
+    console.log(id);
+    axios.delete("http://localhost:8000/experience/" + id).then(() => {
+      console.log(`Deleted post: ${id}`);
+      setLoadedExp("");
+    });
+  };
 
-    let { postId } = useParams();
-    let post = postsdata.find((p) => p.title === postId);
+  const forceReload = () => {
+    window.location.href = "http://localhost:3000/landing-page";
+  };
 
-    return (
-        <Container>
-            <Col>
-                <Row>
-                    <h1>{post.title}</h1>
-                </Row >
-                <Row>
-                    <h3>{post.author}</h3>
-                </Row>
-                <p>{post.shortDes}</p>
-                <p>{post.location}</p>
-                <p>{post.date}</p>
-                <img src={post.imageUrl} alt={post.title} width="200" />
-                <img src={post.imageUrl} alt={post.title}  width="200" />
-                <img src={post.imageUrl} alt={post.title}  width="200" />
-            </Col>
-            <p>{post.story}</p>
-            <div>
-            <Button variant="warning" size="smd">
-                        <Link to="/landing-page"> Back to experiences.
-                            </Link>
-                        </Button>
-            </div>
-        </Container>
+  useEffect(() => {
+    if (!loadedExp) {
+      axios
+        .get("http://localhost:8000/experience/" + postId)
+        .then((response) => {
+          setLoadedExp(response.data);
+        });
+    }
+  });
+
+  let exp = undefined;
+
+  if (postId) {
+    exp = <h1>Loading</h1>;
+  }
+
+  if (loadedExp) {
+    exp = (
+      <Container>
+        <Col>
+          <Row>
+            <h1>{loadedExp.title}</h1>
+          </Row>
+          <Row>
+            <h3>{loadedExp.author}</h3>
+          </Row>
+          <p>{loadedExp.shortDes}</p>
+          <p>{loadedExp.location}</p>
+          <p>{loadedExp.date}</p>
+          <img src={loadedExp.imageUrl} alt={loadedExp.title} width="200" />
+          <img src={loadedExp.imageUrl} alt={loadedExp.title} width="200" />
+          <img src={loadedExp.imageUrl} alt={loadedExp.title} width="200" />
+        </Col>
+        <p>{loadedExp.story}</p>
+        <div>
+          <Button variant="warning" size="smd">
+            <Link to="/landing-page">Back to experiences.</Link>
+          </Button>
+        </div>
+        <div>
+          <Button
+            variant="warning"
+            size="smd"
+            onClick={() => {
+              deleteHandler(loadedExp.id);
+            }}
+          >
+            Delete experience
+          </Button>
+        </div>
+      </Container>
     );
-}
+  } else {
+    exp = (
+      <div>
+        <h1>Deleted post</h1>
+        <Button onClick={() => forceReload()} variant="warning" size="smd">
+          Back to experiences
+        </Button>
+      </div>
+    );
+  }
+  return exp;
+};
 export default SinglePage;
