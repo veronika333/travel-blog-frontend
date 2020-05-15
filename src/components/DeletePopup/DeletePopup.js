a7f1eae43fb5db8ae3f3dededc64ab1171693988
veronika333 <vera3@inbox.ru>
2020-05-15 10:57:13 +0300

    bootstrap

--- src/components/DeletePopup/DeletePopup.js
+++ src/components/DeletePopup/DeletePopup.js
@@ -2,30 +2,33 @@ import React from 'react';
 import Modal from "react-bootstrap/Modal";
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./DeletePopup.css";
 import { useRouteMatch, Link, useParams } from "react-router-dom";
 
-const style = {
-    wrapper: {
-        width: '100vw',
-        height: '100vh',
-        position: 'absolute',
-        zIndex: 999,
-        backgroundColor: 'transparent',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        padding: '10vw',
-    },
-    modal : {
-        height: '100%',
-        width: '100%',
-        backgroundColor: 'rgba(155,155,154)',
-    }
+// const style = {
+//     wrapper: {
+//         width: '100vw',
+//         height: '100vh',
+//         position: 'absolute',
+//         backgroundColor: 'transparent',
+//         top: 0,
+//         left: 0,
+//         bottom: 0,
+//         right: 0,
+//         padding: '10vw',
+//     },
+//     modal : {
+//         height: '100%',
+//         width: '100%',
+//         zIndex: 999,
+//         //backgroundColor: 'rgba(155,155,154)',
+//     }
 
-}
+// }
 
 const DeletePopup = (props) => {
+
+
     return (
         // <div style = {style.wrapper}>
         //     <div style = {style.modal}>
@@ -34,19 +37,20 @@ const DeletePopup = (props) => {
         //         <button onClick={props.deleteHandler}>Delete</button>
         //     </div>
         // </div>
-
-        <Modal.Dialog style = {style.wrapper}>
-            <Modal.Header closeButton>
-              <Modal.Title>Warning</Modal.Title>
+<div className="wrapper">
+        <Modal.Dialog className="modal">
+            <Modal.Header>
+              <Modal.Title>Are you sure?</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              <p>Are you sure you want to delete the post?</p>
+              <p>Are you sure that you want to delete permanently the selected experience?</p>
             </Modal.Body>
             <Modal.Footer>
-              <Button variant="secondary" onClick={props.modalHandler}>Cancel</Button>
-              <Button variant="primary" onClick={props.deleteHandler}>Accept</Button>
+              <Button variant="warning" size="smd" onClick={props.modalHandler}>Cancel</Button>
+              <Button variant="warning" size="smd" onClick={props.deleteHandler}>Delete</Button>
             </Modal.Footer>
         </Modal.Dialog>
+</div>
     )
 };
 export default DeletePopup;
