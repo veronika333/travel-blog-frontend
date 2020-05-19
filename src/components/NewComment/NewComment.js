5de67cb125a3d4a42eb031ddb5e8113440e75436
otsolap <59653348+otsolap@users.noreply.github.com>
2020-05-19 10:11:57 +0300

    {add-comment-form} proof reading

--- src/components/NewComment/NewComment.js
+++ src/components/NewComment/NewComment.js
@@ -22,19 +22,18 @@ const NewComment = () => {
         commentConfirm: null,
     });
 
-
-    const changeCValuehandler = (c) => {
+    const changeCommentValueHandler = (k) => {
         setNewComment({
             ...newComment,
-            [c.target.name]: c.target.value,
+            [k.target.name]: k.target.value,
         });
     };
 
-    const addCommentHandler = (c) => {
-        c.preventDefault();
+    const addCommentHandler = (k) => {
+        k.preventDefault();
 
         // sends new Experience post (NewComment) from browser and database.
-        axios.post("http://localhost:5000/experience/:id/comment", NewComment)
+        axios.post("http://localhost:5000/experience/:id/comment", newComment)
             .then(response => {
                 // Here the code gets the message to make the alert visible.
                 setcommentSent({
@@ -47,7 +46,7 @@ const NewComment = () => {
 
                 setcommentSent({
                     disableAlert: false,
-                    commentConfirm: false
+                    commentConfirm: false,
                 });
             });
     }
@@ -59,15 +58,15 @@ const NewComment = () => {
             </div>
             <Form.Group>
                 <Form.Label htmlFor="author">Author</Form.Label>
-                <Form.Control id="author" name="author" type="text" onChange={changeCValuehandler} />
+                <Form.Control id="author" name="author" type="text" onChange={changeCommentValueHandler} />
             </Form.Group>
             <Form.Group>
                 <Form.Label htmlFor="content">Write a Comment</Form.Label>
-                <Form.Control id="content" name="content" as="textarea" rows="2" onChange={changeCValuehandler} />
+                <Form.Control id="content" name="content" as="textarea" rows="2" onChange={changeCommentValueHandler} />
             </Form.Group>
             <Form.Group>
                 <Form.Label htmlFor="imageUrl">Include an image</Form.Label>
-                <Form.Control id="imageUrl" name="imageUrl" type="text" onChange={changeCValuehandler} />
+                <Form.Control id="imageUrl" name="imageUrl" type="text" onChange={changeCommentValueHandler} />
             </Form.Group>
             <Button className="d-inline-block" variant="warning" type="submit">
                 Submit
