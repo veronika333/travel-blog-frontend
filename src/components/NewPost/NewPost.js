1d96ab9c914ba93a45e57904d8723182a1beb14d
veronika333 <vera3@inbox.ru>
2020-05-21 12:16:51 +0300

    nav bar changed

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -59,56 +59,56 @@ const NewPost = () => {
 
   return (
     <Container>
-    <Form className="newPost" onSubmit={addPostHandler}>
-    <h2>Create a new experience</h2>
+      <Form className="newPost" onSubmit={addPostHandler}>
+        <h2>Create a new experience</h2>
         <br />
-      <Form.Group>
-        <Form.Label htmlFor="title">Title</Form.Label>
-        <Form.Control
-          id="title"
-          name="title"
-          type="text"
-          onChange={changeValueHandler}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label htmlFor="imageUrl">Image</Form.Label>
-        <Form.Control
-          id="imageUrl"
-          name="imageUrl"
-          type="text"
-          onChange={changeValueHandler}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label htmlFor="author">Author</Form.Label>
-        <Form.Control
-          id="author"
-          name="author"
-          type="text"
-          onChange={changeValueHandler}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label htmlFor="shortDesc">Description</Form.Label>
-        <Form.Control
-          id="shortDesc"
-          name="shortDesc"
-          as="textarea"
-          rows="2"
-          onChange={changeValueHandler}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label htmlFor="location">Location</Form.Label>
-        <Form.Control
-          id="location"
-          name="location"
-          type="text"
-          onChange={changeValueHandler}
-        /> 
-      </Form.Group>
-      <Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="title">Title</Form.Label>
+          <Form.Control
+            id="title"
+            name="title"
+            type="text"
+            onChange={changeValueHandler}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="imageUrl">Image</Form.Label>
+          <Form.Control
+            id="imageUrl"
+            name="imageUrl"
+            type="text"
+            onChange={changeValueHandler}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="author">Author</Form.Label>
+          <Form.Control
+            id="author"
+            name="author"
+            type="text"
+            onChange={changeValueHandler}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="shortDesc">Description</Form.Label>
+          <Form.Control
+            id="shortDesc"
+            name="shortDesc"
+            as="textarea"
+            rows="2"
+            onChange={changeValueHandler}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="location">Location</Form.Label>
+          <Form.Control
+            id="location"
+            name="location"
+            type="text"
+            onChange={changeValueHandler}
+          />
+        </Form.Group>
+        <Form.Group>
           <Form.Label htmlFor="date">Date</Form.Label>
           <Form.Control
             id="date"
@@ -117,35 +117,31 @@ const NewPost = () => {
             onChange={changeValueHandler}
           />
         </Form.Group>
-      <Form.Group>
-        <Form.Label htmlFor="story">Story</Form.Label>
-        <Form.Control
-          id="story"
-          name="story"
-          as="textarea"
-          rows="4"
-          onChange={changeValueHandler}
-        />
-      </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="story">Story</Form.Label>
+          <Form.Control
+            id="story"
+            name="story"
+            as="textarea"
+            rows="4"
+            onChange={changeValueHandler}
+          />
+        </Form.Group>
 
-      {/* <Button className="d-inline-block" variant="warning" type="submit">
-        Share Experience
-      </Button> */}
-
-<button className="button">
+        <button className="button">
           Share Experience   <FontAwesomeIcon icon={faPaperPlane} className="plane-icon" />
         </button>
 
-      {postSent.disableAlert ? (
-        ""
-      ) : (
-        <Alert
-          variant={postSent.experienceSent === true ? "success" : "danger"}
-        >
-          Experience {postSent.experienceSent === true ? "Posted" : "not Sent"}
-        </Alert>
-      )}
-    </Form>
+        {postSent.disableAlert ? (
+          ""
+        ) : (
+            <Alert
+              variant={postSent.experienceSent === true ? "success" : "danger"}
+            >
+              Experience {postSent.experienceSent === true ? "Posted" : "not Sent"}
+            </Alert>
+          )}
+      </Form>
     </Container>
   );
 };
